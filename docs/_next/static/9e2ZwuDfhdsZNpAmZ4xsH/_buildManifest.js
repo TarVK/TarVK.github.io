07f7b3dae7a27dd1dfd1db6e6ce6efcdae900a76
TarVK <tarvankrieken@gmail.com>
2022-07-02 11:46:12 +0200

    Built the site

--- docs/_next/static/9e2ZwuDfhdsZNpAmZ4xsH/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-ea939aab753d9e9db3bd.js"],"/[[...id]]":["static/chunks/pages/[[...id]]-688bd14b5018ef822aaf.js"],sortedPages:["/_app","/_error","/[[...id]]"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
